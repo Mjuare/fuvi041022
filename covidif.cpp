312fd9d02c290d00f845ba4bc10c225241768354
maycol juare <mjuarez@itv.edu.ni>
2022-10-06 11:45:07 -0600

    prueba covid

---
+++ covidif.cpp
@@ -0,0 +1,84 @@
+/*
+nombre del archivo: covid
+autor:maycol juarez
+lugar:ITV
+instrucciones: 
+*/
+
+#include<iostream>
+
+using namespace std;
+
+int main(){
+	 bool name;
+	 int fiebre;
+	 int tos;
+	 int perdida;
+	 int cansancio;
+	 
+	 
+	 
+	 
+	 cout<<" hola, por favor ingrese su nombre"<<endl;
+	 cin>>name;
+	 
+	 cout<<"hola "<<name<< " por favor responda de la manera que se le solicite"<<endl;
+	 
+	 cout<<" usted tiene fiebre?(si o no)"<<endl;
+	 cin>>fiebre;
+	 
+	 cout<<"usted tiene tos actualmente?(si o no)"<<endl;
+	 cin>>tos;
+	 
+	 cout<<"usted siente algun tipo de cansancio execivo? ( si o no)"<<endl;
+	 cin>>cansancio;
+	 
+	 cout<<" he sufrido una perdida en su sentido del gusto o el olfato?( si o no)"<<endl;
+	 cin>>perdida;
+	 
+	/* if( tos== 'si' && fiebre =='si' && cansancio=='si' && perdida=='si') {
+	 	cout<< " usted tiene covid ";
+	 }
+	if( tos== 'no' && fiebre=='si' && cansancio=='si' && perdida=='si') {
+	 	cout<< " usted tiene covid ";
+	 }
+	if( tos== 'si' && fiebre=='no' && cansancio=='si' && perdida=='si') {
+	 	cout<< " usted tiene covid ";
+	 }
+	if( tos== 'si' && fiebre=='si' && cansancio=='no' && perdida=='si') {
+	 	cout<< " usted tiene covid ";
+	 }
+	if( tos== 'si' && fiebre=='si' && cansancio=='si' && perdida=='no') {
+	 	cout<< " usted tiene covid ";
+	 }
+	if( tos== 'no' && fiebre=='no' && cansancio=='si' && perdida=='si') {
+	 	cout<< " usted tiene covid ";
+	 }
+	if( tos== 'si' && fiebre=='no' && cansancio=='no' && perdida=='si') {
+	 	cout<< " usted tiene covid ";
+	 }
+	if( tos== 'si' && fiebre=='si' && cansancio=='no' && perdida=='no') {
+	 	cout<< " usted tiene covid ";
+	 }
+	if( tos== 'no' && fiebre=='si' && cansancio=='no' && perdida=='si') {
+	 	cout <<" usted tiene covid ";
+	 }
+	if( tos== 'no' && fiebre=='si' && cansancio=='si' && perdida=='no') {
+	 	cout<<" usted tiene covid ";
+	 }
+	if( tos== 'si' && fiebre=='no' && cansancio=='si' && perdida=='no') {
+	 	cout <<" usted tiene covid ";
+	 }
+	 
+	if( tos== 'no' && fiebre=='si' && cansancio=='no' && perdida=='si') {
+	 	cout<<" usted tiene covid ";
+	 }
+	if( tos== 'si' && fiebre=='si' && cansancio=='si' && perdida=='si') {
+	 	cout <<" usted tiene covid ";
+	 }
+	 */
+	
+	
+	
+	return 0;
+}
